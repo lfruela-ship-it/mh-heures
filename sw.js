53b0f8dfcc6e19d6dc7a0b3a093d8066a34e4641
lfruela-ship-it <lfruela@gmail.com>
2026-06-26 16:09:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mh-heures-v18";
+const CACHE = "mh-heures-v19";
 const ASSETS = ["./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
